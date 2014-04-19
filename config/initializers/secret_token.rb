efdb647855281f972f429a7d67de65264de15707
shadysayed <shady.sayed@tagipedia.com>
2014-04-20 00:40:40 +0200

    Initial Create

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Blog::Application.config.secret_key_base = 'ccee08c16b70ec7141a5cafd7a40a8b6f28993ddfdb60e152a988f6f3564a4288a5bf11ff7417952ad0becd3c42322a9142ac8292abfed1314e3b89abccb2615'
